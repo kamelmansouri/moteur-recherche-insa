6d04cae08e52e93f74588e14ff10fa798f951c40
alexis.gregoire@gmail.com <alexis.gregoire@gmail.com@2a37c84d-521e-71dc-57b8-696ed5d75cde>
2011-11-16 13:05:47 +0000

    Ajout de la fonctionnalité prenant en charge la position et la fréquence d'un terme dans une chaîne

---
+++ trunk/MoteurRecherche/src/moteurrecherche/ChaineTraitee.java
@@ -0,0 +1,24 @@
+
+package moteurrecherche;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ChaineTraitee {
+    ArrayList<Terme> listeTermes;
+    HashMap<String, Integer> frequenceTerme;
+    
+    public ChaineTraitee(ArrayList<Terme> l, HashMap<String, Integer> f) {
+        listeTermes     = l;
+        frequenceTerme  = f;
+    }
+
+    public HashMap<String, Integer> getFrequenceTerme() {
+        return frequenceTerme;
+    }
+
+    public ArrayList<Terme> getListeTermes() {
+        return listeTermes;
+    }
+    
+}
